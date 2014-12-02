6fe2577fd34ec2e316bdf466776e979b67a403e4
ashlynnpai <ashlynnpai@gmail.com>
2014-12-02 19:35:59 +0000

    test1

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,9 @@ class Video < ActiveRecord::Base
   
   validates :title, presence: true
   validates :description, presence: true
+  
+  def self.search_by_title(str)
+    where("title LIKE ?", "%#{str}%")
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130161508) do
+ActiveRecord::Schema.define(version: 20141202175243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,13 @@ ActiveRecord::Schema.define(version: 20141130161508) do
   end
 
   create_table "videos", force: true do |t|
-    t.string  "title"
-    t.text    "description"
-    t.string  "small"
-    t.string  "large"
-    t.integer "category_id"
+    t.string   "title"
+    t.text     "description"
+    t.string   "small"
+    t.string   "large"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
