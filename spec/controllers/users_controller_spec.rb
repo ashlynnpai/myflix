77ad49fbbc63c3e51c06ef2fb0a52ef4e355a381
ashlynnpai <ashlynnpai@gmail.com>
2014-12-27 17:08:00 +0000

    mailer tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,13 +9,29 @@ describe UsersController do
   end
   describe "POST create" do
     context "with valid input" do
-      before { post :create, user: Fabricate.attributes_for(:user) }
+      before { post :create, user: Fabricate.attributes_for(:user, email: "test@email.com") }
+      
       it "creates user record" do
         expect(User.count).to eq(1)
       end
       it "redirects to signin" do
         expect(response).to redirect_to login_path
       end
+      
+      it "sends an email" do
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+      
+      it "sends to the right recipient" do
+        message = ActionMailer::Base.deliveries.last
+        message.to.should == ["test@email.com"]
+      end
+      
+      it "has the right content" do
+        message = ActionMailer::Base.deliveries.last
+        message.body.should include('MyFlix has one of the largest selections of videos around.')
+      end
+      
     end
       
     context "with invalid input" do
