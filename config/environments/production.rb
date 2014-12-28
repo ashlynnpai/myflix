7b4a7e70bbf0691b6e21e3ba666a6bcebd8cea89
ashlynnpai <ashlynnpai@gmail.com>
2014-12-28 20:02:34 +0000

    mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,10 +18,12 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
   
+  config.action_mailer.default_url_options = { host: "vpass.herokuapp.com" }
+  
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "example.com",
+  domain: "vpass.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
