b857b28d39cfaea25a0e2b79996e7d37490645f0
ashlynnpai <ashlynnpai@gmail.com>
2015-01-03 00:46:21 +0000

    change mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Myflix::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   
+  config.action_mailer.default_url_options = { host: ENV['dev_host'] }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   user_name: ENV['MAILTRAP_LOGIN'],
