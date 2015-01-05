bb891d2d23b072650ec3e19e7123c2b50b57ffeb
ashlynnpai <ashlynnpai@gmail.com>
2015-01-05 20:58:09 +0000

    add credentials

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -1,5 +1,9 @@
 class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
   process :resize_to_fill => [665, 375] 
 end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -1,5 +1,9 @@
 class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+  
   process :resize_to_fill => [166, 236]
 end
