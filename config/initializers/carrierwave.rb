cfa2e69ac6248d6f848c434d73519c26124e3f00
ashlynnpai <ashlynnpai@gmail.com>
2015-01-05 21:04:21 +0000

    modified credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['AWS_ACCESS'],                        # required
-      :aws_secret_access_key  => ENV['AWS_SECRET'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET']                       # required
     }
     config.fog_directory  = ENV['BUCKET']                         # required
   else
