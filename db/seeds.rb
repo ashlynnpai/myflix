0c7e524f6e7a55fd6ff45d8aac16f79d46a3a297
ashlynnpai <ashlynnpai@gmail.com>
2015-01-24 04:29:46 +0000

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create(name: "TV Comedies")
-Category.create(name: "TV Dramas")
+Category.create(name: "Action")
+Category.create(name: "Comedy")
+Category.create(name: "Drama")
 
 
 
